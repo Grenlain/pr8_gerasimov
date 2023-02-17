3047450f30fadfbde15aa11ab67e8d6baebaa23a
Тагир Герасимов <gerasimovtagir1@gmail.com>
2023-02-17 19:22:30 +0300

    Добавил в репозиторий main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+chars = 'abcdefghijkl'
+number = input('Количество паролей?'+"\n"
+length = input('длина паролей?'+"\n"
+number = int(number)
+length = int(length)
+for n in range(number):
+    password =''
+    for i in range(lenght):
+        password += random.choice(chars)
+    print(password)
+
